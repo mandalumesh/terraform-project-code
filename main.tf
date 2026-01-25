34b096c2ac5bfbff018adcdf385d4334c943c4a2
mandalumesh <um3514653@gmail.com>
2026-01-25 12:23:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,14 @@
+# resource "azurerm_resource_group" "RG" {
+#   name     = "motu-rg"
+#   location = "westus2"
+
+# }
+
+# resource "azurerm_storage_account" "storage" {
+#   name = "motustorage"
+#   location = "westus2"
+#   resource_group_name = "motu-rg"
+#   account_tier = "Standard"
+#   account_replication_type = "LRS"
+  
+# }
